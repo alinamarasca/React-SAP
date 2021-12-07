84fbfca96462151bf9c2cafa8e06cd5d0ab15a8c
HYF <alinamarasca@gmail.com>
2021-12-07 19:10:54 +0100

    redirect to new post

--- app/components/CreatePost.js
+++ app/components/CreatePost.js
@@ -1,19 +1,23 @@
 import React, { useEffect, useState } from "react";
 import Page from "./Page";
 import Axios from "axios";
+import { withRouter } from "react-router-dom";
 
-function CreatePost() {
+function CreatePost(props) {
   const [title, setTitle] = useState();
   const [body, setBody] = useState();
 
   async function handleSubmit(e) {
     e.preventDefault();
     try {
-      await Axios.post("/create-post", {
+      const response = await Axios.post("/create-post", {
         title,
         body,
         token: localStorage.getItem("complexAppToken")
       });
+      //Redirect to new post url
+      props.history.push(`/post/${response.data}`);
+      console.log("New post created");
     } catch (e) {
       console.log("There was a problem");
     }
@@ -56,4 +60,4 @@ function CreatePost() {
   );
 }
 
-export default CreatePost;
+export default withRouter(CreatePost);
